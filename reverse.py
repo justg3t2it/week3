838c4d63e7e40af462d50278b2309ce73ac1a737
justg3t2it <86816710+justg3t2it@users.noreply.github.com>
2021-08-29 02:50:19 -0700

    updated reverse/reverse test

--- reverse.py
+++ reverse.py
@@ -1,5 +1,55 @@
-def reverseReverse(n): 
-    return int(str(n)[::-1])
+import sys 
+
+def reverseReverse(st):
+    if st == "":
+        return st 
+    
+    x = len(st)
+    reversed = [0] * x
+    
+
+    for i in range(len(st)):
+        if (st[i] != "," and st[i] != "."): 
+            reversed[i] = st[i]
+        else:
+            reversed[i] = ""
+            
+    listStr = ''.join([str(elem) for elem in reversed])
+    listStr = listStr[::-1]
+    print(listStr)
+
+    for x in range(len(st)):
+        if (st[x] == ","):
+            listStr = listStr[:x] + "," + listStr[x:] 
+        if (st[x] == "."):
+            listStr = listStr[:x] + "." + listStr[x:]
+ 
+    return listStr
+
+
+
+    
+
+        
+    
+    
+    
+
+        
+    
+
+
+
+
+
+      
+
+
+
+
+
+
+
 
 
 
--- reverse_test.py
+++ reverse_test.py
@@ -2,14 +2,14 @@ import pytest
 from reverse import reverseReverse
 
 def testFirst():
-    assert reverseReverse(123) == 321
+    assert reverseReverse("123") == "321"
 
 def testSecond():
-    assert reverseReverse(0) == 0
+    assert reverseReverse("0") == "0"
 
 def testThird():
-    assert reverseReverse(2,400) == 4,2
+    assert reverseReverse("3,200") == "23"
 
 def testFourth():
-    assert reverseReverse(11,245.2) == 2,542.1
+    assert reverseReverse("123,4") == "432,1"
 
